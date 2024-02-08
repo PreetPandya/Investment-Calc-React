85abeebd28c3ca6d294f615ff076fac7de0ee136
pandya3033 <pandyapreet456@gmail.com>
2024-02-08 15:06:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header";
+import { useState } from "react";
 
 function App() {
+  const [initialInvestment, setInitialInvestment] = useState(10000);
+  const [annualInvestment, setAnnualInvestment] = useState(1200);
+  const [expectedReturn, setExpectedReturn] = useState(6);
+  const [duration, setDuration] = useState(10);
+  const [result, setResult] = useState();
+
+  function calculateFV() {
+    if (initialInvestment === 0 && annualInvestment === 0 && expectedReturn === 0 && duration === 0) {
+      alert("All values cannot be 0");
+      return;
+    }
+
+    const newResult = [];
+
+    for (let i = 1; i <= duration; i++) {
+      const part1 = Math.pow(1 + expectedReturn / 100, i);
+      const lhs = initialInvestment * part1;
+      const rhs = annualInvestment * ((part1 - 1) / (expectedReturn / 100));
+      const futureValue = lhs + rhs;
+      newResult.push({ i, value: futureValue.toFixed(2) });
+    }
+
+    setResult(newResult);
+  }
+
+  function resetValues() {
+    setInitialInvestment(10000);
+    setAnnualInvestment(1200);
+    setExpectedReturn(6);
+    setDuration(10);
+    setResult([]);
+  }
+
+  const formatCurrency = (value) => {
+    // Assuming you want to format the value as ₹ with commas for thousands separator
+    return new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' }).format(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <Header />
+      <div className="container">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>Initial Investment</label>
+          <input
+            min="0"
+            type="number"
+            required
+            placeholder="Enter Value"
+            value={initialInvestment}
+            onChange={(e) =>
+              setInitialInvestment(parseFloat(e.target.value) || 0)
+            }
+          />
+          <label>Annual Investment</label>
+          <input
+            min="0"
+            type="number"
+            required
+            placeholder="Enter Value"
+            value={annualInvestment}
+            onChange={(e) =>
+              setAnnualInvestment(parseFloat(e.target.value) || 0)
+            }
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          <label>Expected Return (%)</label>
+          <input
+            min="0"
+            type="number"
+            required
+            placeholder="Enter Value"
+            value={expectedReturn}
+            onChange={(e) => setExpectedReturn(parseFloat(e.target.value) || 0)}
+          />
+          <label>Duration (Years)</label>
+          <input
+            min="0"
+            type="number"
+            required
+            placeholder="Enter Value"
+            value={duration}
+            onChange={(e) => setDuration(parseFloat(e.target.value) || 0)}
+          />
+        </p>
+        <p className="btn">
+          <button onClick={calculateFV}>Calculate</button>
+          <button id="reset" onClick={resetValues}>Reset</button>
+        </p>
+        {result &&
+          result.map((item) => (
+            <p className="list" key={item.i}>
+              Year: {item.i} Future Value: {formatCurrency(item.value)}
+            </p>
+          ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+export default function Header(){
+    return(
+        <header>
+            <img id="logoimg" src="logo1.png" alt="A Canvas" />
+            <h1>Investment Calculator</h1>
+        </header>
+    );
+}
+
+
